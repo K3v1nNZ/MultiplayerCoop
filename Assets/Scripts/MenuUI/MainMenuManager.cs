5c765f4e0c4ea06cabb9815f8c1d8b84665dd103
Kevin <84693622+K3v1nNZ@users.noreply.github.com>
2024-06-20 15:05:55 +1200

    basic main menu bottom bar

--- Assets/Scripts/MenuUI/MainMenuManager.cs
+++ Assets/Scripts/MenuUI/MainMenuManager.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Threading.Tasks;
 using DG.Tweening;
 using Game.Networking;
+using Steamworks;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Color = Steamworks.Data.Color;
+using Image = Steamworks.Data.Image;
 
 namespace Game.MenuUI
 {
@@ -10,6 +17,8 @@ namespace Game.MenuUI
         [SerializeField] private CanvasGroup menuCanvasGroup;
         [SerializeField] private CanvasGroup menuButtonsCanvasGroup;
         [SerializeField] private CanvasGroup hostJoinCanvasGroup;
+        [SerializeField] private TMP_Text profileName;
+        [SerializeField] private RawImage profilePicture;
         
         private void Awake()
         {
@@ -22,6 +31,35 @@ namespace Game.MenuUI
         private void Start()
         {
             fadeCanvasGroup.DOFade(0f, 1f).SetEase(Ease.Linear);
+            profileName.text = SteamClient.Name;
+            GetProfilePicture();
+        }
+
+        private async void GetProfilePicture()
+        {
+            Image? image;
+            try
+            {
+                image = await SteamFriends.GetLargeAvatarAsync(SteamClient.SteamId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+
+            var avatar = new Texture2D((int)image.Value.Width, (int)image.Value.Height, TextureFormat.ARGB32, false);
+            avatar.filterMode = FilterMode.Trilinear;
+            for (int x = 0; x < image.Value.Width; x++)
+            {
+                for (int y = 0; y < image.Value.Height; y++)
+                {
+                    Color p = image.Value.GetPixel(x, y);
+                    avatar.SetPixel(x, (int)image.Value.Height - y, new UnityEngine.Color(p.r / 255.0f, p.g / 255.0f, p.b / 255.0f, p.a / 255.0f));
+                }
+            }
+            avatar.Apply();
+            profilePicture.texture = avatar;
         }
 
         #region MainMenuButtons
